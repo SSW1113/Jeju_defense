32a57f00998905a26cdb13e50e1d13365ee224d9
조정현 <emforhs0913@naver.com>
2024-10-09 16:45:06 +0900

    fix: port번호 3000번
    - 기존의 포트번호인 3005번에서 3000번으로 포트 변경

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from "express";
+import dotenv from 'dotenv';
 import { createServer } from "http";
+import initSocket from "./init/socket.js";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
 
-const PORT = 3005;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
 initSocket(server);
 
 server.listen(PORT, async () => {
