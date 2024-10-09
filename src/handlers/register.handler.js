0fa26876044aa7b8768e22b223049f2ff38530b0
조정현 <emforhs0913@naver.com>
2024-10-09 16:28:36 +0900

    feat: registerHandler
    - 클라가 연결된 이후 호출되는 함수 선언
    - uuid 생성 및 다앙햔 이벤트를 소켓에 등록 가능

---
+++ src/handlers/register.handler.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+import { handleConnection, handleDisconnect, handlerEvent } from "./helper.js";
+
+/*---------------------------------------------
+    [ProcessConnect]
+
+    - 클라가 연결된 이후 호출
+    - uuid 생성 및 다양한 이벤트 소켓에 등록
+---------------------------------------------*/
+const registerHandler = (io) =>{
+    io.on('connection', (socket)=>{
+        
+        //uuid 생성
+        const userUUID = uuidv4();
+
+        //클라에게 정상적으로 연결됐다고 알림
+        handleConnection(socket, userUUID);
+
+        //사용자 정의 이벤트 등록
+        socket.on('event', (data)=>handlerEvent(io, socket, data));
+
+        //접속해제 이벤트 등록
+        socket.on('disconnect', ()=>handleDisconnect(socket.id, userUUID));
+
+    })
+}
+
+export default registerHandler;
