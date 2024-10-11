d1227d0844e13bd4a442c8d123872b8c36aea44b
조정현 <emforhs0913@naver.com>
2024-10-11 17:23:56 +0900

    feat: registerHandler에 몬스터 스포너 생성
    클라 연결 시 몬스터 스포너 생성
    #6

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import { handleConnection, handleDisconnect, handlerEvent } from "./helper.js";
+import { sessionManager } from "../models/session.model.js";
+import { monsterManager } from "../models/monsterSpawner.model.js";
 
 /*---------------------------------------------
     [ProcessConnect]
@@ -16,12 +18,15 @@ const registerHandler = (io) =>{
         //클라에게 정상적으로 연결됐다고 알림
         handleConnection(socket, userUUID);
 
+        
         //사용자 정의 이벤트 등록
         socket.on('event', (data)=>handlerEvent(io, socket, data));
-
+        
         //접속해제 이벤트 등록
         socket.on('disconnect', ()=>handleDisconnect(socket.id, userUUID));
-
+        
+        let session = sessionManager.register(userUUID, socket);
+        monsterManager.addSpanwer(session)
     })
 }
 
