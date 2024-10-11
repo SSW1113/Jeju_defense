79bea98ea7b363ca4f55c9f41550a7910ac96a4c
조정현 <emforhs0913@naver.com>
2024-10-11 16:25:23 +0900

    feat: server코드에 packet.js추가
    -몬스터 생성 기능 추가

---
+++ src/utils/packet.js
@@ -0,0 +1,26 @@
+/*---------------------------------------------
+    [PacketID]
+        1. SyncGameState: 게임상태 동기화
+        2. RecoverGameState: 게임 상태 복구(이어하기)
+        3. 몬스터 생성(서버->클라)
+
+    [주의사항] 
+        - 대문자로 시작하기
+        - issue번호에 따라 만들기
+        -S2C: Server->Client 전송
+        -C2S: Client->Sever전송
+---------------------------------------------*/
+export const ePacketId = {
+    SyncGameState: 1,
+    RecoverGameState: 2,
+    S2CGenMonster: 3,
+  };
+  
+  export class Packet {
+    constructor(packetId, userId, clientVersion, payload) {
+      this.packetId = packetId; //요청을 처리할 서버 핸들러의 ID
+      this.userId = userId; //요청을 보내는 유저의 ID
+      this.clientVersion = clientVersion; //현재 클라이언트 버전 (”1.0.0”) (고정)
+      this.payload = payload; // JSON 데이터, 요청 내용
+    }
+  }
