62ed7f2a87100df9f6aecb5ffd6accae4cd993e6
Bzarre <32144764@dankook.ac.kr>
2024-10-12 18:00:24 +0900

    removeBase

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,3 +1,4 @@
+import { removeBase } from "../models/base.model.js";
 import { redis } from "../utils/redis/index.js";
 
 export const gameEnd = (uuid, payload) => {
@@ -7,6 +8,8 @@ export const gameEnd = (uuid, payload) => {
     if (baseHp > 0) {
         return { status: 'fail', message: 'Base HP remains' };
     }
+    removeBase(uuid);
+
     //점수 체크
     let FinalScore = 0;
 
@@ -15,9 +18,9 @@ export const gameEnd = (uuid, payload) => {
     if (score !== FinalScore) {
         return { status: 'fail', message: 'Score mismatch' }
     }
-
+    //redis 모르겠다
     if (score > redis.HighScore) {
-        
+
     }
 
     return { status: 'success' };
