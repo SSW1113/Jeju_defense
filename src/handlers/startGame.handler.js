a8d30d040acfc15b46f39e3efa3b7f6abc7e43c3
조정현 <emforhs0913@naver.com>
2024-10-14 03:31:22 +0900

    feat: 상태 동기화(게임 시작)
    - 게임 시작 시, 2000골드 지급
    - 골드 변경을 클라에게 전송하기 위함

--- src/handlers/startGame.handler.js
+++ src/handlers/startGame.handler.js
@@ -5,5 +5,5 @@ export const startGame = (uuid, payload) => {
   redis.set(`user:${uuid}:gold`, 2000);
   console.log(`Redis: 유저 ${uuid}의 골드 2000G로 초기화`);
 
-  return { packetId: ePacketId.S2CStartGame, data: 2000 };
+  return { status: 'success', packetId: ePacketId.S2CStartGame, payload: 2000 };
 };
