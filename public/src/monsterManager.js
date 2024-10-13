c4f3d388d912de9870f2ce0060ddd45009bfb8d4
조정현 <emforhs0913@naver.com>
2024-10-14 03:32:28 +0900

    comment
    주석 처리된 구 버전 코드 제거

--- public/src/monsterManager.js
+++ public/src/monsterManager.js
@@ -2,40 +2,47 @@ import { utils } from "../utils/utils.js";
 import { assetManager } from "./init/AssetManager.js";
 import { Monster } from "./monster.js";
 
+const NUM_OF_MONSTERS = 5; // 몬스터 개수
+
 /*---------------------------------------------
     [Monster Manager]
 
         -목적: 몬스터 생성 및 생성된 몬스터 관리
         -장점: 모듈화
 ---------------------------------------------*/
-const NUM_OF_MONSTERS = 5; // 몬스터 개수
-
 class MonsterManager{
     constructor(){
       this.monsters = new Array();
       this.monsterImages= new Array();
 
+      //몬스터 이미지 로딩 파트
       for (let i = 1; i <= NUM_OF_MONSTERS; i++) {
         const img = new Image();
         img.src = `images/monster${i}.png`;
         this.monsterImages.push(img);
       }
     }
-     spawnMonster(monsterId, level){
-      let newMonStat = assetManager.getMonsterStatOrNull(monsterId);
-      try {
-        let newMon = new Monster(utils.getPath(), this.monsterImages[monsterId], monsterId, level, newMonStat.maxHp, newMonStat.hp, newMonStat.attackPower, newMonStat.speed, newMonStat.goldDrop);
-    
-        this.monsters.push(newMon);
-        
-      } catch (error) {
-        console.log(error);
-      }
-    }
+/*---------------------------------------------
+[몬스터 생성]
 
-    getMonsters(){
-      return this.monsters;
+    -목적: 몬스터 생성
+    -장점: 모듈화
+---------------------------------------------*/
+  spawnMonster(monsterId, level){
+    let newMonStat = assetManager.getMonsterStatOrNull(monsterId);
+    try {
+      let newMon = new Monster(utils.getPath(), this.monsterImages[monsterId], monsterId, level, newMonStat.maxHp, newMonStat.hp, newMonStat.attackPower, newMonStat.speed, newMonStat.goldDrop);
+  
+      this.monsters.push(newMon);
+      
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  getMonsters(){
+    return this.monsters;
+  }
 }
 
 export const monsterManager = new MonsterManager;
