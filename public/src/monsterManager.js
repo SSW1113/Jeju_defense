8a579f5322b0ac0ac8d6305809929e05d8e664e6
SSA <158470963+shinseongan@users.noreply.github.com>
2024-10-14 13:51:29 +0900

    Feat: Monster 데이터 추가

---
+++ public/src/monsterManager.js
@@ -0,0 +1,51 @@
+import { getGameAssets } from '../Assets.js';
+import { utils } from '../utils/utils.js';
+import { Monster } from './monster.js';
+
+/*---------------------------------------------
+    [Monster Manager]
+
+        -목적: 몬스터 생성 및 생성된 몬스터 관리
+        -장점: 모듈화
+---------------------------------------------*/
+const NUM_OF_MONSTERS = 5; // 몬스터 개수
+
+class MonsterManager {
+  constructor() {
+    this.monsters = new Array();
+    this.monsterImages = new Array();
+
+    for (let i = 1; i <= NUM_OF_MONSTERS; i++) {
+      const img = new Image();
+      img.src = `images/monster${i}.png`;
+      this.monsterImages.push(img);
+    }
+  }
+  spawnMonster(monsterId, level) {
+    // 수정 필요
+    let newMonStat = getGameAssets();
+    try {
+      let newMon = new Monster(
+        utils.getPath(),
+        this.monsterImages[monsterId],
+        monsterId,
+        level,
+        newMonStat.maxHp,
+        newMonStat.hp,
+        newMonStat.attackPower,
+        newMonStat.speed,
+        newMonStat.goldDrop,
+      );
+
+      this.monsters.push(newMon);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getMonsters() {
+    return this.monsters;
+  }
+}
+
+export const monsterManager = new MonsterManager();
